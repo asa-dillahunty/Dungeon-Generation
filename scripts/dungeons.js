901f8bf829a78314dc0bb8aa79b5106284659136
asa-dillahunty <stantondillahunty@gmail.com>
2020-12-27 01:40:22 -0600

    :video_game: Sprite now moves
    Sprite moves but I need to fix controls. Should definitely find a tutorial online for that
    
    TODO:
    - Use RequestAnimationFrame instead of current interval thing. That will completely change how code is organized. Is pain

--- scripts/dungeons.js
+++ scripts/dungeons.js
@@ -1,5 +1,10 @@
+/**
+ * @author Asa Dillahunty
+ */
+
 var interval = null;
 renderDungeon();
+
 
 document.getElementById("liveSection").style.display = "inherit";
 
@@ -12,36 +17,10 @@ function renderDungeon() {
 
 	var size = 2;
 
-	var c = {
-		x:0,
-		y:0,
-		height: size * scale,
-		width: size * scale,
-		color: "#0000FF",
-		draw: function (context) {
-			drawCell(context,this);
-		}
-	};
-	var b = {
-		x:0,
-		y:0,
-		height: size * scale,
-		width: size * scale,
-		color: "#FFFF00",
-		draw: function (context) {
-			drawCell(context,this);
-		}
-	};
-	var a = {
-		x:0,
-		y:0,
-		height: size * scale,
-		width: size * scale,
-		color: "#FF0000",
-		draw: function (context) {
-			drawCell(context,this);
-		}
-	}
+	var a = buildCell("#0000FF", 0, 0, size*scale, size*scale);
+	var b = buildCell("#FFFF00", 0, 0, size*scale, size*scale);
+	var c = buildCell("#FF0000", 0, 0, size*scale, size*scale);
+	
 
 	while (anyOverlap([a,b,c])) {
 		newPoints(c,canvas);
@@ -50,17 +29,24 @@ function renderDungeon() {
 	}
 
 	var sprite = {
-		width: 160,
-		height: 160,
+		width: 50,
+		height: 50,
 		imgW: 160,
 		imgH: 160,
+		cord: {x: 0, y: 0},
+		velocity: {x:0, y:0},
+		pace: 5,
 		img: new Image(),
 		count: 0,
 		offset: 200,
 		draw: function(context) {
-			context.drawImage(this.img, this.offset*this.count, 0, this.imgW, this.imgH, 0, 0, this.width, this.height);
+			context.drawImage(this.img, this.offset*this.count, 0, this.imgW, this.imgH, this.cord.x, this.cord.y, this.width, this.height);
 			this.count = (this.count + 1)%5;
 		},
+		move: function() {
+			this.cord.x += this.pace*this.velocity.x;
+			this.cord.y += this.pace* this.velocity.y;
+		}
 	}
 	sprite.img.src = './images/G7.png';
 
@@ -71,9 +57,36 @@ function renderDungeon() {
 		renderCanvas([a,b,c,sprite], canvas);
 		// if (count == 0) ctx.drawImage(sprite,0,0,160,182,0,0,160,182);
 		// else ctx.drawImage(sprite,200,0,160,182,0,0,160,182);
-	}, 300);
+	}, 100);
+
+	document.addEventListener('keydown', function(event) {
+		if (event.code === "KeyW") sprite.velocity.y = -1;
+		else if (event.code === "KeyS") sprite.velocity.y = 1;
+		else if (event.code === "KeyA") sprite.velocity.x = -1;
+		else if (event.code === "KeyD") sprite.velocity.x = 1;
+	});
+
+	document.addEventListener('keyup', function(event) {
+		if (event.code === "KeyW") sprite.velocity.y = 0;
+		else if (event.code === "KeyS") sprite.velocity.y = 0;
+		else if (event.code === "KeyA") sprite.velocity.x = 0;
+		else if (event.code === "KeyD") sprite.velocity.x = 0;
+	});
 }
 
+function buildCell(color, x, y, width, height) {
+	return {
+		x: x,
+		y: y,
+		height: height,
+		width: width,
+		color: color,
+		draw: function (context) {
+			drawCell(context,this);
+		},
+		move: function(){}
+	};
+}
 
 function anyOverlap(cells) {
 	// test every combination
@@ -113,8 +126,11 @@ function drawCell(ctx, cell) {
 function renderCanvas(objs, canvas) {
 	const context = canvas.getContext('2d');
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = "#000000";
+	context.fillRect(0,0,canvas.width,canvas.height);
 
 	for (var i=0;i<objs.length;i++) {
+		objs[i].move();
 		objs[i].draw(context);
 	}
 }
